e627f8b4f922fb041406d74636125895298b4d1a
Talon Meyer <talonmeyer12@gmail.com>
2023-01-09 21:28:20 -0800

    Added to js file again

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var starter = document.querySelector("#start");
 var answerArr = ["JavaScript", "function", "modulus", "Event Listener", "timerInterval"];
 var index = 0;
 
+var numWins = 0;
+var numLosses = 0;
+
 var timeLeft = 10;
 function countdown() {
     var timerInterval = setInterval(function() {
@@ -14,22 +17,32 @@ function countdown() {
         timeLeft--;
         if (timeLeft === 0) {
             timerInterval(clearInterval);
-
+            numLosses++;
         }
     }, 1000);
 }
 
 function start() {
+    var count = 0;
     input.addEventListener("keydown", function(event) {
         var key = event.key
         for (var i = 0; i < answerArr[index].length; i++) {
             if (key === answerArr[index].charAt(i)) {
                 input.textContent = input.value + key;
+                count++;
             }
         }
     });
+    if (count === answerArr[index].length) {
+        numWins++;
+        winCount.textContent = numWins;
+    } else {
+        numLosses++;
+        lossCount.textContent = numLosses;
+    }
+    index++;
 }
 
-starter.addEventListener("click"), function(event) {
+starter.addEventListener("click", function(event) {
     start();
-}
+});
