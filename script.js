e94444ed9b3f377bcc531109ddafc99f00557534
Talon Meyer <talonmeyer12@gmail.com>
2023-01-09 21:22:24 -0800

    Added to js file

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-var winCount = 0;
-var lossCount = 0;
-var answerArr = ["JavaScript", "function", "modulus", "Event Listener", "timerInterval"];
+var input = document.querySelector("#input");
+var winCount = document.querySelector("#win-count");
+var lossCount = document.querySelector("#loss-count");
+var counter = document.querySelector("#countdown");
+var starter = document.querySelector("#start");
 
+var answerArr = ["JavaScript", "function", "modulus", "Event Listener", "timerInterval"];
+var index = 0;
 
 var timeLeft = 10;
 function countdown() {
     var timerInterval = setInterval(function() {
+        counter.textContent = timeLeft;
         timeLeft--;
         if (timeLeft === 0) {
             timerInterval(clearInterval);
+
         }
     }, 1000);
 }
 
-inputArea.addEventListener("keydown", function(event) {
+function start() {
+    input.addEventListener("keydown", function(event) {
+        var key = event.key
+        for (var i = 0; i < answerArr[index].length; i++) {
+            if (key === answerArr[index].charAt(i)) {
+                input.textContent = input.value + key;
+            }
+        }
+    });
+}
 
-});
+starter.addEventListener("click"), function(event) {
+    start();
+}
